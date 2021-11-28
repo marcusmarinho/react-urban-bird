c355cf03d8fbcad99e9c0b3dff01f9bc86d5f4c5
marcus.marinho <marcuspratamarinho@gmail.com>
2021-11-28 14:53:11 -0300

    add error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { FC } from "react";
+import { useNavigate } from "react-router";
 
-export default () => {
-    return (
-        <div>
-            <p>Error</p>
-        </div>
-    )
-}
+import CustomButton from "../../components/Button/Button";
+
+import "./Error.scss";
+
+const ErrorPage: FC = function (props) {
+  let navigate = useNavigate();
+  function teste() {
+    console.log("navegou");
+    navigate(`/diversao`);
+  }
+
+  return (
+    <div className="c-erro">
+      <div className="c-msg">
+        <p>Algo aconteceu e não conseguimos apresentar a pagina.</p>
+        <p>Tente novamente mais tarde</p>
+      </div>
+      <div className="c-icon"></div>
+      <CustomButton label="Voltar" onClickFunc={teste} />
+    </div>
+  );
+};
+
+export default ErrorPage;
